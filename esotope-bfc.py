f868219a84dc024cfeb0f57e1ffcc136a223e1f8
Kang Seonghoon <public+hg@mearie.org>
2009-05-03 03:01:45 +0900

    AdjustMemory renamed to MemoryOps. some fixes to Expr classes. generic MemoryOps are proven to be hard to implement: disabled tightloop pass for now.

--- esotope-bfc.py
+++ esotope-bfc.py
@@ -15,31 +15,62 @@ class Expr(object):
     __metaclass__ = _ExprMeta
     __slots__ = ['root']
 
-    class Number(namedtuple('Number', 'value')):
+    class _exprbase(tuple):
+        def __eq__(self, rhs):
+            return self.__class__ is rhs.__class__ and tuple.__eq__(self, rhs)
+        def __ne__(self, rhs):
+            return self.__class__ is not rhs.__class__ or tuple.__ne__(self, rhs)
+
+    class Number(namedtuple('Number', 'value'), _exprbase):
         def __str__(self): return str(self.value)
         def __repr__(self): return repr(self.value)
+        def apply(self, func, merge=None): return func(self)
 
-    class MemoryRef(namedtuple('MemoryRef', 'offset')):
+    class MemoryRef(namedtuple('MemoryRef', 'offset'), _exprbase):
         def __str__(self): return 'mptr[%s]' % self.offset
         def __repr__(self): return 'mptr[%r]' % self.offset
+        def apply(self, func, merge=None): return func(self)
 
-    class Add(namedtuple('Add', 'lhs rhs')):
+    class Negate(namedtuple('Negate', 'expr'), _exprbase):
+        def __str__(self): return '-%s' % (self.expr,)
+        def __repr__(self): return '-%r' % (self.expr,)
+        def apply(self, func, merge=None):
+            return (merge or Expr.Negate)(self.expr.apply(func, merge))
+
+    class Add(namedtuple('Add', 'lhs rhs'), _exprbase):
         def __str__(self): return '(%s + %s)' % (self.lhs, self.rhs)
         def __repr__(self): return '(%r+%r)' % (self.lhs, self.rhs)
+        def apply(self, func, merge=None):
+            return (merge or Expr.Add)(self.lhs.apply(func, merge),
+                    self.rhs.apply(func, merge))
 
-    class Subtract(namedtuple('Subtract', 'lhs rhs')):
+    class Subtract(namedtuple('Subtract', 'lhs rhs'), _exprbase):
         def __str__(self): return '(%s - %s)' % (self.lhs, self.rhs)
         def __repr__(self): return '(%r-%r)' % (self.lhs, self.rhs)
+        def apply(self, func, merge=None):
+            return (merge or Expr.Subtract)(self.lhs.apply(func, merge),
+                    self.rhs.apply(func, merge))
 
     class Multiply(namedtuple('Multiply', 'lhs rhs')):
         def __str__(self): return '(%s * %s)' % (self.lhs, self.rhs)
         def __repr__(self): return '(%r*%r)' % (self.lhs, self.rhs)
+        def apply(self, func, merge=None):
+            return (merge or Expr.Multiply)(self.lhs.apply(func, merge),
+                    self.rhs.apply(func, merge))
 
-    def __init__(self, root):
-        if isinstance(root, (int, long)):
-            self.root = Expr.Number(root)
+    def __init__(self, obj=0):
+        if isinstance(obj, (int, long)):
+            self.root = Expr.Number(obj)
+        elif isinstance(obj, Expr):
+            self.root = obj.root
         else:
-            self.root = root
+            self.root = obj
+
+    def __neg__(self):
+        return Expr(Expr.Negate(self.root))
+
+    def __pos__(self):
+        return self
 
     def __add__(self, rhs):
         if rhs == 0: return self
@@ -50,14 +81,39 @@ class Expr(object):
         return Expr(Expr.Add(Expr(lhs).root, self.root))
 
     def __sub__(self, rhs):
+        if rhs == 0: return self
         return Expr(Expr.Subtract(self.root, Expr(rhs).root))
+
     def __rsub__(self, lhs):
+        if lhs == 0: return -self
         return Expr(Expr.Subtract(Expr(lhs).root, self.root))
 
     def __mul__(self, rhs):
+        if rhs == 0: return Expr()
+        if rhs == 1: return self
+        if rhs == -1: return -self
         return Expr(Expr.Multiply(self.root, Expr(rhs).root))
+
     def __rmul__(self, lhs):
+        if lhs == 0: return Expr()
+        if lhs == 1: return self
+        if lhs == -1: return -self
         return Expr(Expr.Multiply(Expr(lhs).root, self.root))
+
+    def __eq__(self, rhs): return self.root == Expr(rhs).root
+    def __ne__(self, rhs): return self.root != Expr(rhs).root
+
+    def references(self):
+        def func(node):
+            if not isinstance(node, Expr.MemoryRef): return set()
+            return set([node.offset])
+        return self.root.apply(func, set.union)
+
+    def movepointer(self, delta):
+        def func(node):
+            if not isinstance(node, Expr.MemoryRef): return node
+            return Expr.MemoryRef(node.offset + delta)
+        return Expr(self.root.apply(func))
 
     def __str__(self):
         return str(self.root)
@@ -72,10 +128,10 @@ def insert_indent(s):
 
 class Node(object):
     def __nonzero__(self):
-        return True # return True if this node is no-op.
+        return True # ... or return False if this node is no-op.
 
     def combine(self, next):
-        pass # return None or combined Node object.
+        return None # ... or return combined Node object.
 
     def __str__(self): raise NotImplemented('should be overriden')
     def __repr__(self): raise NotImplemented('should be overriden')
@@ -92,10 +148,13 @@ class ComplexNode(Node, list):
         while i < len(self):
             if not self[i-1]:
                 del self[i-1]
-            elif self[i-1].combine(self[i]):
-                del self[i]
             else:
-                i += 1
+                combined = self[i-1].combine(self[i])
+                if combined is not None:
+                    self[i-1] = combined
+                    del self[i]
+                else:
+                    i += 1
         if self and not self[0]:
             del self[0]
 
@@ -112,57 +171,47 @@ class Program(ComplexNode):
     def __repr__(self):
         return self._repr('Program')
 
-class SetCurrent(Node):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return '*mptr = %s;\n' % self.value
-
-    def __repr__(self):
-        return 'SetCurrent[%r]' % self.value
-
-class AdjustMemory(Node):
+class MemoryOps(Node, list):
     def __init__(self, offset=0, changes=None):
         self.offset = offset
-        if changes:
-            self.changes = dict(changes)
-            for k, v in self.changes.items():
-                if v == 0: del self.changes[k]
-        else:
-            self.changes = {}
+        try:
+            self.changes = [(k, False, v) for k, v in changes.items()]
+        except:
+            self.changes = changes or []
 
     def __nonzero__(self):
         return len(self.changes) > 0 or self.offset != 0
 
-    def __getitem__(self, offset):
-        assert isinstance(offset, (int, long))
-        return self.changes.get(offset, 0)
+    def get(self, offset):
+        value = Expr[offset]
+        for k, set, v in self.changes:
+            if k == offset:
+                if set: value = v
+                else: value += v
+        return value
 
-    def __setitem__(self, offset, delta):
-        assert isinstance(offset, (int, long))
-        if delta == 0:
-            try: del self.changes[offset]
-            except KeyError: pass
-        else:
-            self.changes[offset] = delta
+    def set(self, offset, value):
+        self.changes.append((offset, False, value))
+
+    def adjust(self, offset, delta):
+        if delta != 0:
+            self.changes.append((offset, True, delta))
 
     def combine(self, next):
-        if isinstance(next, AdjustMemory):
-            for k, v in next.changes.items():
-                self[self.offset + k] += v
+        if isinstance(next, MemoryOps):
+            offset = self.offset
+            for k, set, v in next.changes:
+                self.changes.append((offset + k, set, v.movepointer(offset)))
             self.offset += next.offset
-            return True
-
-        if isinstance(next, SetCurrent):
-            self.offset
-            return False
+            return self
 
     def __str__(self):
         codes = []
 
-        for k, v in self.changes.items():
-            if v < 0:
+        for k, set, v in self.changes:
+            if set:
+                codes.append('mptr[%d] = %s;\n' % (k, v))
+            elif v < 0:
                 codes.append('mptr[%d] -= %s;\n' % (k, -v))
             elif v != 0:
                 codes.append('mptr[%d] += %s;\n' % (k, v))
@@ -175,12 +224,17 @@ class AdjustMemory(Node):
         return ''.join(codes)
 
     def __repr__(self):
-        items = sorted(self.changes.items())
-        if items:
-            return 'AdjustMemory[offset=%r, %s]' % (
-                    self.offset, ', '.join('%d:%r' % (k,v) for k, v in items))
-        else:
-            return 'AdjustMemory[offset=%r]' % self.offset
+        items = []
+        for k, set, v in sorted(self.changes.items()):
+            if set:
+                items.append('%d=%s' % (k, v))
+            elif v < 0:
+                items.append('%d+=%s' % (k, v))
+            elif v != 0:
+                items.append('%d-=%s' % (k, v))
+        if self.offset != 0:
+            items.append('offset=%r' % self.offset)
+        return 'MemoryOps[%s]' % ', '.join(items)
 
 class Input(Node):
     def __str__(self):
@@ -210,23 +264,23 @@ class Compiler(object):
     def parse(self, fp):
         nodestack = [Program()]
 
-        memchanges = {}
-        ptroffset = 0
+        changes = {}
+        offset = 0
         for lineno, line in enumerate(fp):
             for ch in line:
                 if ch == '+':
-                    memchanges[ptroffset] = memchanges.get(ptroffset, 0) + 1
+                    changes[offset] = changes.get(offset, 0) + 1
                 elif ch == '-':
-                    memchanges[ptroffset] = memchanges.get(ptroffset, 0) - 1
+                    changes[offset] = changes.get(offset, 0) - 1
                 elif ch == '>':
-                    ptroffset += 1
+                    offset += 1
                 elif ch == '<':
-                    ptroffset -= 1
+                    offset -= 1
                 elif ch in '.,[]':
-                    if ptroffset != 0 or any(memchanges.values()):
-                        nodestack[-1].append(AdjustMemory(ptroffset, memchanges))
-                    memchanges = {}
-                    ptroffset = 0
+                    if offset != 0 or any(changes.values()):
+                        nodestack[-1].append(MemoryOps(offset, changes))
+                    changes = {}
+                    offset = 0
 
                     if ch == '.':
                         nodestack[-1].append(Output())
@@ -240,14 +294,14 @@ class Compiler(object):
                         loop = nodestack.pop()
                         nodestack[-1].append(loop)
 
-        if ptroffset != 0 or any(memchanges.values()):
-            nodestack[-1].append(AdjustMemory(ptroffset, memchanges))
+        if offset != 0 or any(changes.values()):
+            nodestack[-1].append(MemoryOps(offset, changes))
         if len(nodestack) != 1:
             raise ValueError('Premature end of the loop')
 
         return nodestack[0]
 
-    # tries to optimize tight loop: LoopWhile[AdjustMemory[offset=0, 0:+-1, ...]].
+    # tries to optimize tight loop: LoopWhile[MemoryOps[0+=1/0-=1, ..., offset=0]].
     def optimize_tightloop(self, node):
         if not isinstance(node, ComplexNode):
             return node
@@ -257,7 +311,7 @@ class Compiler(object):
         inodes = []
         for inode in node[:]:
             if isinstance(inode, LoopWhile) and len(inode) == 1 and \
-                    isinstance(inode[0], AdjustMemory) and inode[0].offset == 0:
+                    isinstance(inode[0], MemoryOps) and inode[0].offset == 0:
                 adjustnode = inode[0]
                 if adjustnode[0] == 1:
                     mult = overflow - Expr[0]
@@ -290,7 +344,7 @@ def main(argv):
 
     compiler = Compiler()
     node = compiler.parse(file(argv[1], 'r'))
-    node = compiler.optimize_tightloop(node)
+    #node = compiler.optimize_tightloop(node)
     print node
     return 0
 
